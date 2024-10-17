124808ca580cd41b48a4259388d13ddb1c8c7355
Adão Gonçalves <adao-goncalves@outlook.com>
2024-10-17 11:30:57 +0100

    Update ft_ultimate_ft.c

--- C Projects/C01/ex01/ft_ultimate_ft.c
+++ C Projects/C01/ex01/ft_ultimate_ft.c
@@ -2,36 +2,25 @@ void	ft_ultimate_ft(int *********nbr)
 {
 	*********nbr = 42;
 }
-/*
-#include <stdio.h>
 
-int main()
-{
-	int n;
-	int *nbr8;
-	int **nbr7;
-	int ***nbr6;
-	int ****nbr5;
-	int *****nbr4;
-	int ******nbr3;
-	int *******nbr2;
-	int ********nbr1;
-	int *********nbr;
 
-	n = 21;
-	
-	nbr8 = &n;
-	nbr7 = &nbr8;
-	nbr6 = &nbr7;
-	nbr5 = &nbr6;
-	nbr4 = &nbr5;
-	nbr3 = &nbr4;
-	nbr2 = &nbr3;
-	nbr1 = &nbr2;
-	nbr = &nbr1;
-	
-	ft_ultimate_ft(nbr);
-	printf("%d", n);	
-	return 0;
-}
-*/
+
+--- Explanation ---
+
+English:
+
+    void ft_ultimate_ft(int *********nbr): //Declares the ft_ultimate_ft function, which takes a pointer to a pointer 
+					   //to a pointer to a pointer to a pointer to a pointer to a pointer to a pointer 
+					   //to a pointer to an integer nbr as an argument and returns no values (void).
+
+    *********nbr = 42;: //Dereferences the pointer nbr nine times and sets the value it points to as 42.
+
+
+
+Português:
+
+    void ft_ultimate_ft(int *********nbr): //Declaração da função ft_ultimate_ft, que aceita um ponteiro para um ponteiro 
+					   //para um ponteiro para um ponteiro para um ponteiro para um ponteiro para um ponteiro 
+					   //para um ponteiro para um inteiro nbr como argumento e não retorna valores (void).
+
+    *********nbr = 42;: //Desreferencia o ponteiro nbr nove vezes e define o valor que ele aponta como 42.
