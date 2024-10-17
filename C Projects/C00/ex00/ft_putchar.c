032dd1bd1013542dfbb971a89c12b5e1ee3de4da
Adão Gonçalves <adao-goncalves@outlook.com>
2024-10-17 11:53:57 +0100

    Update ft_putchar.c

--- C Projects/C00/ex00/ft_putchar.c
+++ C Projects/C00/ex00/ft_putchar.c
@@ -10,20 +10,36 @@ void	ft_putchar(char c)
 
 --- Explanation ---
 
-English:
+    Include Directive:
+        #include <unistd.h>: This line includes the <unistd.h> header file, which is necessary for using the write() function. 
+	This function allows the program to write data to file descriptors, including standard output.
 
-    #include <unistd.h>: //Includes the library to use the write() function.
+    Function Declaration:
+        void ft_putchar(char c): This line declares a function named ft_putchar. The function takes one argument, c, which 
+	is a character (char). The function does not return any value (void).
 
-    void ft_putchar(char c): //Declares the ft_putchar function, which takes a character as an argument 
-			     //and returns no values (void).
+    Writing a Character:
+        write(1, &c, 1): This line uses the write() function to output the character passed to ft_putchar. The parameters are:
+            1: This indicates the file descriptor for standard output (the terminal).
+            &c: This is the address of the character c that we want to write. The & operator is used to obtain the memory 
+		address of the variable.
+            1: This specifies that we want to write one byte, which is the size of the character c.
 
-    write(1, &c, 1): //Uses the write() function to send the character stored in c to the terminal (standard output).
 
+	
 Português:
 
-    #include <unistd.h>: //Inclui a biblioteca para usar a função write().
+    Diretiva de Inclusão:
+        #include <unistd.h>: Esta linha inclui o arquivo de cabeçalho <unistd.h>, que é necessário para usar a função write(). 
+	Esta função permite que o programa escreva dados em descritores de arquivos, incluindo a saída padrão.
 
-    void ft_putchar(char c): //Declaração da função ft_putchar, que aceita um caractere como 
-			     //argumento e não retorna valores (void).
+    Declaração da Função:
+        void ft_putchar(char c): Esta linha declara uma função chamada ft_putchar. A função aceita um argumento, c, 
+	que é um caractere (char). A função não retorna nenhum valor (void).
 
-    write(1, &c, 1): //Usa a função write() para enviar o caractere armazenado em c para o terminal (saída padrão).
+    Escrevendo um Caractere:
+        write(1, &c, 1): Esta linha usa a função write() para enviar o caractere passado para ft_putchar. Os parâmetros são:
+            1: Este indica o descritor de arquivo para a saída padrão (o terminal).
+            &c: Este é o endereço do caractere c que queremos escrever. O operador & é usado para obter o 
+		endereço de memória da variável.
+            1: Este especifica que queremos escrever um byte, que é o tamanho do caractere c.
