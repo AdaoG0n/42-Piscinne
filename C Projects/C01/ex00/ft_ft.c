9afe206af08a6baf4f7dfa515d8974abfab38758
Adão Gonçalves <adao-goncalves@outlook.com>
2024-10-17 11:29:20 +0100

    Update ft_ft.c

--- C Projects/C01/ex00/ft_ft.c
+++ C Projects/C01/ex00/ft_ft.c
@@ -2,3 +2,20 @@ void	ft_ft(int *nbr)
 {
 	*nbr = 42;
 }
+
+
+--- Explanation ---
+
+English:
+
+    void ft_ft(int *nbr): //Declares the ft_ft function, which takes a pointer to an integer nbr as an argument and returns no values (void).
+
+    *nbr = 42;: //Dereferences the pointer nbr and sets the value it points to as 42.
+
+
+
+Português:
+
+    void ft_ft(int *nbr): //Declaração da função ft_ft, que aceita um ponteiro para um inteiro nbr como argumento e não retorna valores (void).
+
+    *nbr = 42;: //Desreferencia o ponteiro nbr e define o valor que ele aponta como 42.
