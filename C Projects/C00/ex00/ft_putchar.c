7b0538406db1c2b03c9c6c4c8f097d3fe75e3a26
Adão Gonçalves <adao-goncalves@outlook.com>
2024-10-17 11:03:08 +0100

    Update ft_putchar.c

--- C Projects/C00/ex00/ft_putchar.c
+++ C Projects/C00/ex00/ft_putchar.c
@@ -4,3 +4,15 @@ void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
+
+/*
+#include <unistd.h>: Includes the necessary library to use the write() function.
+	
+void ft_putchar(char c): Declaration of the ft_putchar function, which accepts a character 
+			 as an argument and returns nothing (void).
+	
+write(1, &c, 1): Calls the write function to send 1 byte (the character c) to file descriptor 1, 
+		 which represents standard output (the terminal). The first argument 1 indicates standard output,
+		 the second argument &c is the address of the character to be sent, and the third argument 1 indicates
+		 that only 1 byte (the character) will be sent.
+*/
